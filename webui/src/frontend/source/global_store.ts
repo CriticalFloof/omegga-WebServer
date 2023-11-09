dd492b5100296a6b5ff62a40e6a0ece7ccb7b553
CriticalFloof <zackbig2005@gmail.com>
2023-11-08 23:21:47 -0500

    Created Global Store Class
    Should hopefully allow for easy cross component communication.

---
+++ webui/src/frontend/source/global_store.ts
@@ -0,0 +1,22 @@
+import { EventEmitter } from "events";
+
+export default class GlobalStore extends EventEmitter {
+    private storage: { [index: string]: any } = {};
+
+    constructor() {
+        super();
+    }
+
+    public get(key: string): any {
+        return this.storage[key];
+    }
+
+    public set(key: string, value: any): void {
+        this.storage[key] = value;
+        this.emit(`key_value_changed:${key}`);
+    }
+
+    public onKeyValueChanged(key: string, listener: (...args: any[]) => void) {
+        this.on(`key_value_changed:${key}`, listener);
+    }
+}
