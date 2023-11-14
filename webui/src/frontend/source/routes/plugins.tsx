9043db3bfb6ad79c038a9ccf3a020a6496b3bb4b
CriticalFloof <zackbig2005@gmail.com>
2023-11-14 11:23:17 -0500

    Make PluginList Actually Fetch Plugins.

--- webui/src/frontend/source/routes/plugins.tsx
+++ webui/src/frontend/source/routes/plugins.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { socket } from "../application.tsx";
+import { IPluginSummary } from "../../../web_server_types";
 
 /*
 PLUGINS
@@ -15,21 +17,58 @@ PLUGINS
     -PluginInspector (Children of the plugins route is displayed here.)
 */
 
-// TODO: PUT INTO COMPONENT
+// TODO: PUT INTO COMPONENT FILE
 function OmeggaInput(props: { placeholder?: string }) {
     return <input placeholder={props.placeholder}></input>;
 }
 
-// TODO: PUT INTO COMPONENT
-function OmeggaButton(props: { children?: any }) {
-    return <button>{props.children}</button>;
+// TODO: PUT INTO COMPONENT FILE
+function OmeggaButton({ children = null, ...attributes }) {
+    return <button {...attributes}>{children}</button>;
 }
 
 function PluginsList() {
+    let [isLoaded, updateLoadStatus] = useState<boolean>(false);
+    let [plugins, updatePlugins] = useState<React.JSX.Element[]>([]);
+
+    function getPluginList(): Promise<React.JSX.Element[]> {
+        return new Promise((resolve) => {
+            socket.once("pluginslist:get", (pluginList: IPluginSummary[]) => {
+                console.log(pluginList);
+
+                resolve(
+                    pluginList.map((p) => {
+                        return (
+                            <div>
+                                <span>{p.name}</span>
+                            </div>
+                        );
+                    })
+                );
+            });
+
+            socket.emit("pluginslist:get");
+        });
+    }
+
+    // This is to avoid an infinite loop.
+    if (!isLoaded) {
+        getPluginList().then((pluginList) => updatePlugins(pluginList));
+
+        updateLoadStatus(true);
+    }
+
     return (
         <div>
-            <OmeggaInput placeholder="Search Plugins..." />
-            <OmeggaButton></OmeggaButton>
+            <div>
+                <OmeggaInput placeholder="Search Plugins..." />
+                <OmeggaButton
+                    onClick={async () => {
+                        updatePlugins(await getPluginList());
+                    }}
+                ></OmeggaButton>
+            </div>
+            <div>{plugins}</div>
         </div>
     );
 }
